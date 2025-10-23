39686e0c969108e446fa14df0d851a7edb37013d
Alessandro Amici <a.amici@bopen.eu>
2025-10-24 01:04:48 +0200

    Start test alembic

--- tests/test_10_schema.py
+++ tests/test_10_schema.py
@@ -1,5 +1,13 @@
+import pytest_alembic
+
 from dbapp import schema
+
+history_1 = pytest_alembic.create_alembic_fixture({"file": "alembic.ini"})
 
 
 def test_metadata_create_all(engine):
     schema.metadata.create_all(bind=engine)
+
+
+def test_alembic_upgrade(history_1):
+    pytest_alembic.tests.test_upgrade(history_1)
